1d5488bb718669fa1e4e3a004fc9d743f80586d7
Hafsa Shabbeer <hafsashabbeer@gmail.com>
2026-07-03 11:15:15 -0400

    Implemented stock checking by Hafsa Shabbeer

--- InventoryTracker.py
+++ InventoryTracker.py
@@ -7,3 +7,8 @@ class InventoryTracker:
 
     def display_inventory(self):
         return self.inventory
+
+    def checkStockLevel(self, item_name):
+        if item_name in self.inventory:
+            return self.inventory[item_name]
+        return "Item not found"
